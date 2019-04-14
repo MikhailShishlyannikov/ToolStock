7a7ccc7f63bf9a467975084ac5a9acfa7003c77a
Mikhail <mikhailshishlyannikov@gmail.com>
2019-04-15 00:20:16 +0300

    Changing RoutConfig for areas

--- Sam.ToolStock.Web/App_Start/RouteConfig.cs
+++ Sam.ToolStock.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Sam.ToolStock.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new []{ "Sam.ToolStock.Web.Controllers" }
             );
         }
     }

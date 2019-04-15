e5cfa68439afefed6d67e9f89b50e5be6be61e67
Mikhail <mikhailshishlyannikov@gmail.com>
2019-04-15 22:01:58 +0300

    the template was added

--- Sam.ToolStock.Web/App_Start/BundleConfig.cs
+++ Sam.ToolStock.Web/App_Start/BundleConfig.cs
@@ -8,9 +8,54 @@ namespace Sam.ToolStock.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Template/css")
+                .Include(
+                    "~/Content/normalize.css",
+                    "~/Content/bootstrap.css",
+                    //"~/Content/font-awesome.css",
+                    //"~/Content/themify-icons.css",
+                    //"~/Content/pe-icon-7-stroke.css",
+                    "~/Content/flag-icon.css",
+                    "~/Content/cs-skin-elastic.css",
+                    "~/Content/style.css",
+                    "~/Content/chartist.css",
+                    "~/Content/jqvmap.css",
+                    "~/Content/weather-icons.css",
+                    "~/Content/fullcalendar.css",
+                    "~/Content/style.css",
+                    "~/Content/style.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/Template/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js",
+                "~/Scripts/modernizr-{version}.js",
+                "~/Scripts/bootstrap.bundle.js",
+                "~/Scripts/jquery.matchHeight.js",
+                "~/Scripts/main.js",
+                "~/Scripts/Chart.bundle.js",
+                "~/Scripts/chartist.js",
+                "~/Scripts/chartist-plugin-legend.js",
+                "~/Scripts/jquery.flot.js",
+                "~/Scripts/jquery.flot.pie.min.js",
+                "~/Scripts/jquery.flot.spline.js",
+                "~/Scripts/jquery.simpleWeather.js",
+                "~/Scripts/weather-init.js",
+                "~/Scripts/moment.js",
+                "~/Scripts/fullcalendar.js",
+                "~/Scripts/fullcalendar-init.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/jquery-{version}.js",

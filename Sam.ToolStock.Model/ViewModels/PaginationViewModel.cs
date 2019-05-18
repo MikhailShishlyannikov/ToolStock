36288d6075fe5d268acded43e31dfe8826a4d5a0
Mikhail <mikhailshishlyannikov@gmail.com>
2019-05-18 04:23:03 +0300

    Search for tools

--- Sam.ToolStock.Model/ViewModels/PaginationViewModel.cs
+++ Sam.ToolStock.Model/ViewModels/PaginationViewModel.cs
@@ -10,6 +10,12 @@ namespace Sam.ToolStock.Model.ViewModels
         public int TotalItems { get; set; }
         public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / PageSize);
 
+        public string searchString { get; set; }
+        public string Manufacturer { get; set; }
+        public IList<string> Manufacturers { get; set; }
+        public string toolTypeId { get; set; }
+        public IList<ToolTypeViewModel> ToolTypes { get; set; }
+
         public IEnumerable<ToolCountViewModel> ToolCountViewModels { get; set; }
     }
 }
--- Sam.ToolStock.Web/Areas/Admin/Controllers/ToolController.cs
+++ Sam.ToolStock.Web/Areas/Admin/Controllers/ToolController.cs
@@ -111,17 +111,51 @@ namespace Sam.ToolStock.Web.Areas.Admin.Controllers
             return View("ModalMessage", message);
         }
 
-        public ActionResult ShowAll(int page = 1, int pageSize = 6)
+        public ActionResult ShowAll(
+            int page = 1, 
+            int pageSize = 6, 
+            string searchString = "",
+            string manufacturer = "Choose a manufacturer", 
+            string toolTypeId = "Choose a tool type"
+            )
         {
 
             var tools = _toolService.GetAllToolCounts().ToList();
+            var toolTypes = _toolTypeService.GetAll().ToList();
+            var manufacturers = tools.Select(t => t.Manufacturer).Distinct().ToList();
+
+
+            if (searchString != "")
+            {
+                tools = tools.Where(t => t.Name.Contains(searchString)).ToList();
+            }
+            if (manufacturer != "Choose a manufacturer")
+            {
+                tools = tools.Where(t => t.Manufacturer == manufacturer).ToList();
+            }
+            if (toolTypeId != "Choose a tool type")
+            {
+                tools = tools.Where(t => t.ToolTypeName == toolTypes.FirstOrDefault(tt => tt.Id == toolTypeId)?.Name).ToList();
+                toolTypes.Insert(0, new ToolTypeViewModel { Id = "Choose a tool type", Name = "Choose a tool type" });
+            }
+            else
+            {
+                toolTypes.Insert(0, new ToolTypeViewModel { Id = toolTypeId, Name = toolTypeId });
+            }
+
+            manufacturers.Insert(0, "Choose a manufacturer");
+
             var toolsPerPages = tools.Skip((page - 1) * pageSize).Take(pageSize).ToList();
             var paginationViewModel = new PaginationViewModel
             {
                 PageNumber = page,
                 PageSize = pageSize,
                 TotalItems = tools.Count(),
-                ToolCountViewModels = toolsPerPages
+                ToolCountViewModels = toolsPerPages,
+                Manufacturer = manufacturer,
+                Manufacturers = manufacturers,
+                toolTypeId = toolTypeId,
+                ToolTypes = toolTypes
             };
 
             return View(paginationViewModel);
